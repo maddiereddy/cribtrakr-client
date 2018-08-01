a367e694fcdbce55cce64294c234a767dac7d9f9
maddiereddy <maddiereddy@gmail.com>
2018-07-31 17:36:12 -0700

    finish add expense component

---
+++ src/components/add-expense-form.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { newExpense  } from '../actions/expenses';
+import './dashboard.css';
+import Input from './input';
+import {Categories} from '../data';
+import Textarea from './textarea';
+
+
+export class AddExpenseForm extends React.Component {
+  
+  onSubmit(values) {
+    const username = this.props.username;
+    let property = this.props.rentals.find(rental => {
+      return rental.name === values.propName
+    })
+    if (!property) property = this.props.rentals[0];
+
+    const expense = Object.assign({}, {user: username}, {propId: property.id}, values);
+    return this.props.dispatch(newExpense(expense));
+  }
+
+  render() {
+    let categories = Categories;
+    const categoriesOptions = categories.map((category, index) => 
+      <option key={index} value={category.value}>{category.value}</option>
+    );
+
+    const rentalsOptions = this.props.rentals.map((rental, index) => 
+      <option key={index} value={rental.name}>{rental.name}</option>
+    );
+
+    return (
+        <div>
+          <form className="add-expense-form" id="edit-property-form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))} >
+          
+          <section className="property-details">
+            <fieldset className="form-section">
+              <label htmlFor="propName">Property:</label>
+              <Field component="select" name="propName" required>{rentalsOptions}</Field>
+              <label htmlFor="category">Category:</label>
+              <Field component="select" name="category" required>{categoriesOptions}</Field>
+              <label htmlFor="date">Date of Service: </label>
+              <Field component="input" type="date" name="date" required />
+              <label htmlFor="amount">Expense: </label>
+              <Field component={Input} type="text" name="amount" required />
+              <label htmlFor="vendor">Vendor: </label>
+              <Field component={Input} type="text" name="vendor" required />
+              <label htmlFor="description">Description: </label>
+              <Field component={Textarea} type="text" name="description" required />
+            </fieldset>
+          </section>
+          <div>
+            <Link to="/expenses"><button type="button">Back</button></Link>
+            <button type="submit" disabled={this.props.pristine || this.props.submitting} >
+              Add Expense
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+AddExpenseForm = reduxForm({
+    form: "addExpense",
+    // onSubmitFail: (errors, dispatch) =>
+    //     dispatch(focus('editRental', Object.keys(errors)[0]))
+})(AddExpenseForm);
--- src/components/add-expense.js
+++ src/components/add-expense.js
@@ -2,88 +2,39 @@ import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import {fetchProtectedData} from '../actions/protected-data';
+import {fetchRentals} from '../actions/rentals';
 import './dashboard.css';
 import Header from './header';
+import { AddExpenseForm } from './add-expense-form';
+import spinner from '../images/ajax-loader.gif';
 
 export class AddExpense extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchProtectedData());
-    // this.fileSelector = document.getElementById('selectedFile');
+    this.props.dispatch(fetchRentals());
   }
 
-  // handleFileSelect = (e) => {
-  //   e.preventDefault();
-  //   this.fileSelector.click();
-  // }
-
   render() {
+    if (this.props.loading) 
+      return <div id="loading"><img src={spinner} alt="Loading..."/></div>;
+      
     return (
       <div className="dashboard">
         <Header title='Add New Expense' />
         <section>
-        <form id="add-expense-form">
-          <section className="expense-details">
-            <label htmlFor="property">Property:</label>
-            <select className="drop-down" name="property" required>
-              <option value="select" defaultValue>Select Property</option>
-              <option value="Property1">Property 1</option>
-              <option value="Property2">Property 2</option>
-              <option value="Property3">Property 3</option>
-            </select>
-            
-            <label htmlFor="category">Category:</label>
-            <select className="drop-down" name="category" required>
-              <option value="select" defaultValue>Select Category</option>
-              <option value="advertising">Advertising</option>
-              <option value="travel">Auto and Travel</option>
-              <option value="cleaning">Cleaning and Maintenance</option>
-              <option value="commissions">Commissions</option>
-              <option value="fines">Fines</option>
-              <option value="insurance">Insurance</option>
-              <option value="legal">Legal and other Professional Fees</option>
-              <option value="management">Management Fees</option>
-              <option value="mortgage">Mortgage Interest</option>
-              <option value="otherInterest">Other Interest</option>
-              <option value="repairs">Repairs</option>
-              <option value="supplies">Supplies</option>
-              <option value="taxes">Taxes</option>
-              <option value="utilities">Utilities</option>
-              <option value="yardwork">Yard Work</option>
-              <option value="other">Other</option>
-            </select>
-            
-            <label htmlFor="amount">Amount:</label>
-            <input type="text" name="amount" required />
-            <label htmlFor="vendor">Vendor:</label>
-            <input type="text" name="vendor" />
-            <label htmlFor="description">Descr:</label>
-            <textarea name="description"></textarea>
-            <label htmlFor="date">Date:</label>
-            <input type="date" name="date" required />
-            {/* <div className="upload-pic">
-              <input id="selectedFile" type="file" onChange={readURL(this)}/>
-              <input type="button" value="Upload Image" onClick={this.handleFileSelect} />
-              <br />
-              <div className="pic-container">
-                <img src={require("../images/receipt.png")} alt="Receipt" />  
-              </div>
-            </div> */}
-          </section>
-          <button type="button" onClick={this.props.history.goBack}>Back</button>
-          <button type="submit">Save Expense</button>
-        </form>
-      </section>
+        <AddExpenseForm username={this.props.username} rentals={this.props.rentals}/>
+        </section>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const {currentUser} = state.auth;
   return {
-      username: state.auth.currentUser.username,
-      name: `${currentUser.firstName} ${currentUser.lastName}`,
-      protectedData: state.protectedData.data
+    username: state.auth.currentUser.username,
+    protectedData: state.protectedData.data,
+    rentals: state.rental.rentals,
+    loading: state.rental.loading
   };
 };
 
